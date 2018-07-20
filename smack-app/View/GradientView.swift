a3fad8a900a23ab90ff4a394852c74bc2dd1a1f8
jem.sai <jem.sai@whitecloak.com>
2018-07-20 20:00:06 +0800

    Project - Added new file (GradientView.Swift)

---
+++ smack-app/View/GradientView.swift
@@ -0,0 +1,21 @@
+//
+//  GradientView.swift
+//  smack-app
+//
+//  Created by Jemimah Beryl M. Sai on 20/07/2018.
+//  Copyright © 2018 Jemimah Beryl M. Sai. All rights reserved.
+//
+
+import UIKit
+
+class GradientView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
