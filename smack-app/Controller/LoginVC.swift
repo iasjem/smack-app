c470bbca45f5c2130385ac8f2406f8025fccb4a3
jem.sai <jem.sai@whitecloak.com>
2018-07-23 11:39:18 +0800

    LoginVC - Added perform segue for Create account VC

--- smack-app/Controller/LoginVC.swift
+++ smack-app/Controller/LoginVC.swift
@@ -21,4 +21,8 @@ class LoginVC: UIViewController {
     }
     
 
+    @IBAction func createAccountButtonPressed(_ sender: Any) {
+        performSegue(withIdentifier: TO_CREATE_ACCOUNT, sender: nil)
+    }
+    
 }
