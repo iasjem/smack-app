bf24a3bca37f5694f86889e9def8fe2f12fa1737
jem.sai <jem.sai@whitecloak.com>
2018-07-23 18:26:22 +0800

    CreateAccountVC - added segue to avatar picker screen

--- smack-app/Controller/CreateAccountVC.swift
+++ smack-app/Controller/CreateAccountVC.swift
@@ -49,7 +49,7 @@ class CreateAccountVC: UIViewController {
     }
     
     @IBAction func chooseAvatarPressed(_ sender: Any) {
-        
+        performSegue(withIdentifier: TO_AVATAR_PICKER, sender: nil)
     }
     
     @IBAction func generateBGColorButtonPressed(_ sender: Any) {
