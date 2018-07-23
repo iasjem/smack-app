8607a673c356b5a3179df3d1742697980ca4094b
jem.sai <jem.sai@whitecloak.com>
2018-07-23 18:25:55 +0800

    AvatarCell - Added setup for Avatar cells

--- smack-app/View/AvatarCell.swift
+++ smack-app/View/AvatarCell.swift
@@ -9,5 +9,16 @@
 import UIKit
 
 class AvatarCell: UICollectionViewCell {
+    @IBOutlet weak var avatarImage: UIImageView!
     
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setupView()
+    }
+    
+    func setupView() {
+        self.layer.backgroundColor = UIColor.lightGray.cgColor
+        self.layer.cornerRadius = 10.0
+        self.clipsToBounds = true
+    }
 }
