564d53a8d70a17ab15457c6dc3b2c3840c2e971d
jem.sai <jem.sai@whitecloak.com>
2018-07-23 18:24:03 +0800

    Projects - added new files

---
+++ smack-app/View/AvatarCell.swift
@@ -0,0 +1,13 @@
+//
+//  AvatarCell.swift
+//  smack-app
+//
+//  Created by Jemimah Beryl M. Sai on 23/07/2018.
+//  Copyright © 2018 Jemimah Beryl M. Sai. All rights reserved.
+//
+
+import UIKit
+
+class AvatarCell: UICollectionViewCell {
+    
+}
