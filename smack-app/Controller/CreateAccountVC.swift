15f1f1e26d302a8e1f481253bb4d720f64e3c142
jem.sai <jem.sai@whitecloak.com>
2018-07-23 11:40:00 +0800

    CreateAccountVC - Added close button action

--- smack-app/Controller/CreateAccountVC.swift
+++ smack-app/Controller/CreateAccountVC.swift
@@ -29,6 +29,8 @@ class CreateAccountVC: UIViewController {
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
+    @IBAction func closeButtonPressed(_ sender: Any) {
+        performSegue(withIdentifier: UNWIND, sender: nil)
     }
     */
 
