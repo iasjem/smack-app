6c009cc18bd580bbfbdd9487f6712ab5ab4496f3
jem.sai <jem.sai@whitecloak.com>
2018-07-23 17:02:06 +0800

    Constants - added new constant variables

--- smack-app/Utilities/Constants.swift
+++ smack-app/Utilities/Constants.swift
@@ -14,6 +14,7 @@ typealias CompletionHandler = (_ Success: Bool) -> ()
 let TO_LOGIN = "toLogin"
 let TO_CREATE_ACCOUNT = "toCreateAccount"
 let UNWIND = "unwindToChannel"
+let URL_LOGIN = "\(BASE_URL)account/login"
 
 // URL Constants
 let BASE_URL = "https://smack-chat-clone.herokuapp.com/v1/"
@@ -23,4 +24,9 @@ let URL_REGISTER = "\(BASE_URL)account/register"
 let TOKEN_KEY = "token"
 let LOGGED_IN_KEY = "loggedIn"
 let USER_EMAIL = "userEmail"
+
+// Headers
+let HEADER = [
+    "Content-Type": "application/json; charset=utf-8"
+]
 
