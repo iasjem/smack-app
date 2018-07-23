11c897af9cb891abf7b6a5265e480ac372d702ff
jem.sai <jem.sai@whitecloak.com>
2018-07-23 17:37:57 +0800

    CreateAccountVC - Added function to add user data

--- smack-app/Controller/CreateAccountVC.swift
+++ smack-app/Controller/CreateAccountVC.swift
@@ -14,6 +14,9 @@ class CreateAccountVC: UIViewController {
     @IBOutlet weak var emailText: UITextField!
     @IBOutlet weak var passwordText: UITextField!
     
+    var avatarName = "profileDefault"
+    var avatarColor = "[0.5, 0.5, 0.5, 1]"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -24,16 +27,20 @@ class CreateAccountVC: UIViewController {
     }
     
     @IBAction func createAccountButtonPressed(_ sender: Any) {
-        guard let email = emailText.text, emailText.text != "" else
-        {   return  }
-        guard let password = passwordText.text, passwordText.text != "" else
-        {   return  }
+        guard let name = usernameText.text, usernameText.text != "" else {   return  }
+        guard let email = emailText.text, emailText.text != "" else {   return  }
+        guard let password = passwordText.text, passwordText.text != "" else {   return  }
         
         AuthService.instance.registerUser(email: email, password: password) { (success) in
             if success {
                 AuthService.instance.loginUser(email: email, password: password, completion: { (success) in
                     if success {
-                        print("logged in user", AuthService.instance.authToken)
+                        AuthService.instance.createUser(name: name, email: email, avatarName: self.avatarName, avatarColor: self.avatarColor, completion: { (success) in
+                            if success {
+                                print(UserDataService.instance.name, UserDataService.instance.avatarName)
+                                self.performSegue(withIdentifier: UNWIND, sender: nil)
+                            }
+                        })
                     }
                 })
             }
