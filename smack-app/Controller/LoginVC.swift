2db88e6819b289f9d6689f97798c3de6b40d53ae
jem.sai <jem.sai@whitecloak.com>
2018-07-23 11:39:36 +0800

    LoginVC - Refactored code for clarity

--- smack-app/Controller/LoginVC.swift
+++ smack-app/Controller/LoginVC.swift
@@ -13,14 +13,12 @@ class LoginVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
     @IBAction func closeButtonPressed(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
     
-
     @IBAction func createAccountButtonPressed(_ sender: Any) {
         performSegue(withIdentifier: TO_CREATE_ACCOUNT, sender: nil)
     }
