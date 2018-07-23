cf07f32983d5300da7df36772ee7545c56e56db6
jem.sai <jem.sai@whitecloak.com>
2018-07-23 16:34:21 +0800

    Projects - added new file (RoundedButton)

---
+++ smack-app/View/RoundedButton.swift
@@ -0,0 +1,21 @@
+//
+//  RoundedButton.swift
+//  smack-app
+//
+//  Created by Jemimah Beryl M. Sai on 23/07/2018.
+//  Copyright © 2018 Jemimah Beryl M. Sai. All rights reserved.
+//
+
+import UIKit
+
+class RoundedButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
