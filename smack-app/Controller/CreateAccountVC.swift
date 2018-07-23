13350562e199a75f619be9b5a4f0139dec7ec159
jem.sai <jem.sai@whitecloak.com>
2018-07-23 16:36:15 +0800

    CreateAccountVC - Modified create account action logic

--- smack-app/Controller/CreateAccountVC.swift
+++ smack-app/Controller/CreateAccountVC.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class CreateAccountVC: UIViewController {
 
+    @IBOutlet weak var usernameText: UITextField!
+    @IBOutlet weak var emailText: UITextField!
+    @IBOutlet weak var passwordText: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -17,6 +21,27 @@ class CreateAccountVC: UIViewController {
     
     @IBAction func closeButtonPressed(_ sender: Any) {
         performSegue(withIdentifier: UNWIND, sender: nil)
+    }
+    
+    @IBAction func createAccountButtonPressed(_ sender: Any) {
+        guard let email = emailText.text, emailText.text != "" else
+        {   return  }
+        guard let password = passwordText.text, passwordText.text != "" else
+        {   return  }
+        
+        AuthService.instance.registerUser(email: email, password: password) { (success) in
+            if success {
+                print("registered user")
+            }
+        }
+    }
+    
+    @IBAction func chooseAvatarPressed(_ sender: Any) {
+        
+    }
+    
+    @IBAction func generateBGColorButtonPressed(_ sender: Any) {
+        
     }
     
 }
