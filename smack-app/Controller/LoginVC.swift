f416ab3b412ccfa1868d19bcf18117a03fc8560a
jem.sai <jem.sai@whitecloak.com>
2018-07-23 11:07:58 +0800

    LoginVC - added close button action

--- smack-app/Controller/LoginVC.swift
+++ smack-app/Controller/LoginVC.swift
@@ -29,6 +29,8 @@ class LoginVC: UIViewController {
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
+    @IBAction func closeButtonPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
     }
     */
 
