4a1c7bfa3d11f443eb9b69357f4f6eff8dc8f48f
jem.sai <jem.sai@whitecloak.com>
2018-07-23 16:12:10 +0800

    Projects - add new file (AuthService)

---
+++ smack-app/Services/AuthService.swift
@@ -0,0 +1,9 @@
+//
+//  AuthService.swift
+//  smack-app
+//
+//  Created by Jemimah Beryl M. Sai on 23/07/2018.
+//  Copyright © 2018 Jemimah Beryl M. Sai. All rights reserved.
+//
+
+import Foundation
