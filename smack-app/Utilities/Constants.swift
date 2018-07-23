953c91eac1e392ff9b3878fe9c259173fa788d8f
jem.sai <jem.sai@whitecloak.com>
2018-07-23 16:13:41 +0800

    Constants - Refactored code for clarity

--- smack-app/Utilities/Constants.swift
+++ smack-app/Utilities/Constants.swift
@@ -9,11 +9,18 @@
 import Foundation
 
 typealias CompletionHandler = (_ Success: Bool) -> ()
+
+// Segues
 let TO_LOGIN = "toLogin"
 let TO_CREATE_ACCOUNT = "toCreateAccount"
 let UNWIND = "unwindToChannel"
+
+// URL Constants
 let BASE_URL = "https://smack-chat-clone.herokuapp.com/v1/"
 let URL_REGISTER = "\(BASE_URL)account/register"
+
+// User Defaults
 let TOKEN_KEY = "token"
 let LOGGED_IN_KEY = "loggedIn"
 let USER_EMAIL = "userEmail"
+
