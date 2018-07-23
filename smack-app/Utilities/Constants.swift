430b55265c214e6df7b42dde0339a221dc3c831f
jem.sai <jem.sai@whitecloak.com>
2018-07-23 11:06:38 +0800

    Projects - added new files

---
+++ smack-app/Utilities/Constants.swift
@@ -0,0 +1,9 @@
+//
+//  Constants.swift
+//  smack-app
+//
+//  Created by Jemimah Beryl M. Sai on 23/07/2018.
+//  Copyright © 2018 Jemimah Beryl M. Sai. All rights reserved.
+//
+
+import Foundation
