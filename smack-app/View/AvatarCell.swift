fa699fd48a271effaadcc601fbd141c1ff2c7565
jem.sai <jem.sai@whitecloak.com>
2018-07-23 18:56:15 +0800

    AvatarCell - added function to configure  cell

--- smack-app/View/AvatarCell.swift
+++ smack-app/View/AvatarCell.swift
@@ -21,6 +21,16 @@ class AvatarCell: UICollectionViewCell {
         setupView()
     }
     
+    func configureCell(index: Int, type: AvatarType) {
+        if type == AvatarType.dark {
+            avatarImage.image = UIImage(named: "dark\(index)")
+            self.layer.backgroundColor = UIColor.lightGray.cgColor
+        } else {
+            avatarImage.image = UIImage(named: "light\(index)")
+            self.layer.backgroundColor = UIColor.gray.cgColor
+        }
+    }
+    
     func setupView() {
         self.layer.backgroundColor = UIColor.lightGray.cgColor
         self.layer.cornerRadius = 10.0
