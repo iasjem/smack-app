f2934822deb415949c0a5afa26e6d11dc85f1a79
jem.sai <jem.sai@whitecloak.com>
2018-07-24 18:33:27 +0800

    AddChannelVC - setup Add Channel screen

--- smack-app/Controller/AddChannelVC.swift
+++ smack-app/Controller/AddChannelVC.swift
@@ -9,27 +9,34 @@
 import UIKit
 
 class AddChannelVC: UIViewController {
+    
+    @IBOutlet weak var channelNameText: UITextField!
+    @IBOutlet weak var channelDescriptionText: UITextField!
+    @IBOutlet weak var bgView: UIView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        setupView()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func createChannelButtonPressed(_ sender: Any) {
+        
     }
-    */
+    
+    @IBAction func closeButtonPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func setupView() {
+        channelNameText.attributedPlaceholder = NSAttributedString(string: "name", attributes: [NSAttributedStringKey.foregroundColor: smackPurplePlaceholder])
+        channelDescriptionText.attributedPlaceholder = NSAttributedString(string: "description", attributes: [NSAttributedStringKey.foregroundColor: smackPurplePlaceholder])
+        let tap = UITapGestureRecognizer(target: self, action: #selector(handleTap))
+        self.bgView.addGestureRecognizer(tap)
+    }
+    
+    @objc func handleTap() {
+        dismiss(animated: true, completion: nil)
+    }
 
 }
