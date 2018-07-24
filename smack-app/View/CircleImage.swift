326f75e0e49f5f3f4d89a093a6fb95ac68f5fc15
jem.sai <jem.sai@whitecloak.com>
2018-07-24 11:06:09 +0800

    Projects - added new file (CircleImage)

---
+++ smack-app/View/CircleImage.swift
@@ -0,0 +1,13 @@
+//
+//  CircleImage.swift
+//  smack-app
+//
+//  Created by Jemimah Beryl M. Sai on 24/07/2018.
+//  Copyright © 2018 Jemimah Beryl M. Sai. All rights reserved.
+//
+
+import UIKit
+
+class CircleImage: UIImage {
+
+}
