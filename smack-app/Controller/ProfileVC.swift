14f6828f1d4d73472d923b0c226975899b60689a
jem.sai <jem.sai@whitecloak.com>
2018-07-24 14:51:23 +0800

    ProfileVC - added IBOutlets and IBActions

--- smack-app/Controller/ProfileVC.swift
+++ smack-app/Controller/ProfileVC.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ProfileVC: UIViewController {
 
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var userEmailLabel: UILabel!
+    @IBOutlet weak var bgView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -19,6 +23,9 @@ class ProfileVC: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    
+    @IBAction func closeButtonPressed(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
     }
     
 
@@ -29,6 +36,10 @@ class ProfileVC: UIViewController {
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
+    @IBAction func logoutButtonPressed(_ sender: Any) {
+        UserDataService.instance.logoutUser()
+        NotificationCenter.default.post(name: NOTIF_USER_DATA_DID_CHANGE, object: nil)
+        dismiss(animated: true, completion: nil)
     }
     */
 
