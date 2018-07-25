3a5a737bfce01cef61387d06b8559e7c61bdc572
jem.sai <jem.sai@whitecloak.com>
2018-07-25 18:12:47 +0800

    Projects - added new file

---
+++ smack-app/View/MessageCell.swift
@@ -0,0 +1,24 @@
+//
+//  MessageCell.swift
+//  smack-app
+//
+//  Created by Jemimah Beryl M. Sai on 25/07/2018.
+//  Copyright © 2018 Jemimah Beryl M. Sai. All rights reserved.
+//
+
+import UIKit
+
+class MessageCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
