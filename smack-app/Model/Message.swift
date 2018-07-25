9b45d2033b8adc8984815f753fc065bd5d14aa27
jem.sai <jem.sai@whitecloak.com>
2018-07-25 15:29:27 +0800

    Projects - added new files

---
+++ smack-app/Model/Message.swift
@@ -0,0 +1,9 @@
+//
+//  Message.swift
+//  smack-app
+//
+//  Created by Jemimah Beryl M. Sai on 25/07/2018.
+//  Copyright © 2018 Jemimah Beryl M. Sai. All rights reserved.
+//
+
+import Foundation
---
+++ smack-app/Services/SocketService.swift
@@ -0,0 +1,13 @@
+//
+//  SocketService.swift
+//  smack-app
+//
+//  Created by Jemimah Beryl M. Sai on 25/07/2018.
+//  Copyright © 2018 Jemimah Beryl M. Sai. All rights reserved.
+//
+
+import UIKit
+
+class SocketService: NSObject {
+
+}
