02637daa95d224cc213972e9bb7d91f5b87346f5
jem.sai <jem.sai@whitecloak.com>
2018-07-25 18:31:14 +0800

    MessageCell - setup message cell for table view of Chat screen

--- smack-app/View/MessageCell.swift
+++ smack-app/View/MessageCell.swift
@@ -9,16 +9,36 @@
 import UIKit
 
 class MessageCell: UITableViewCell {
-
+    
+    @IBOutlet weak var userImage: CircleImage!
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var timeStampLabel: UILabel!
+    @IBOutlet weak var messageBodyLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    
+    func configureCell(message: Message) {
+        messageBodyLabel.text = message.message
+        userNameLabel.text = message.userName
+        userImage.image = UIImage(named: message.userAvatar)
+        userImage.backgroundColor = UserDataService.instance.returnUIColor(components: message.userAvatarColor)
+        
+        guard var isoDate = message.timeStamp else {  return  }
+        let end = isoDate.index(isoDate.endIndex, offsetBy: -5)
+        isoDate = isoDate.substring(to: end)
+        
+        let isoFormatter = ISO8601DateFormatter()
+        let chatDate = isoFormatter.date(from: isoDate.appending("Z"))
+        
+        let newFormatter = DateFormatter()
+        newFormatter.dateFormat = "MMM d, h:mm a"
+        
+        if let finalDate = chatDate {
+            let finalDate = newFormatter.string(from: finalDate)
+            timeStampLabel.text = finalDate
+        }
     }
 
 }
