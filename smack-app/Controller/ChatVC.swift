a62e1aca386f53d61a107308813fcb78ef9022de
jem.sai <jem.sai@whitecloak.com>
2018-07-25 18:37:20 +0800

    ChatVC - added functionality for displaying number of users typing

--- smack-app/Controller/ChatVC.swift
+++ smack-app/Controller/ChatVC.swift
@@ -14,10 +14,15 @@ class ChatVC: UIViewController {
     @IBOutlet weak var menuButton: UIButton!
     @IBOutlet weak var channelNameLabel: UILabel!
     @IBOutlet weak var messageText: UITextField!
+    @IBOutlet weak var messageSendButton: UIButton!
+    @IBOutlet weak var typingUsersIndicator: UILabel!
+    
+    var isTyping = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.bindToKeyboard()
+        messageSendButton.isHidden = true
         let tap = UITapGestureRecognizer(target: self, action: #selector(handleTap))
         view.addGestureRecognizer(tap)
         menuButton.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
@@ -26,6 +31,42 @@ class ChatVC: UIViewController {
         
         NotificationCenter.default.addObserver(self, selector: #selector(ChatVC.userDataDidChange), name: NOTIF_USER_DATA_DID_CHANGE, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(ChatVC.channelSelected), name: NOTIF_CHANNEL_SELECTED, object: nil)
+        
+        SocketService.instance.getChatMessage { (newMessage) in
+            if newMessage.channelId == MessageService.instance.selectedChannel?.id && AuthService.instance.isLoggedIn {
+                MessageService.instance.messages.append(newMessage)
+                self.tableView.reloadData()
+                if MessageService.instance.messages.count > 0 {
+                    let endIndex = IndexPath(row: MessageService.instance.messages.count - 1, section: 0)
+                    self.tableView.scrollToRow(at: endIndex, at: .bottom, animated: false)
+                }
+            }
+        }
+        
+        SocketService.instance.getTypingUsers { (typingUsers) in
+            guard let channelId = MessageService.instance.selectedChannel?.id else {   return   }
+            var names = ""
+            var numberOfTypers = 0
+            for (typingUser, channel) in typingUsers {
+                if typingUser != UserDataService.instance.name && channel == channelId {
+                    if names == "" {
+                        names = typingUser
+                    } else {
+                        names = "\(names), \(typingUser)"
+                    }
+                    numberOfTypers += 1
+                }
+            }
+            if numberOfTypers > 0 && AuthService.instance.isLoggedIn == true {
+                var verb = "is"
+                if numberOfTypers > 1 {
+                    verb = "are"
+                }
+                self.typingUsersIndicator.text = "\(names) \(verb) typing a message"
+            } else {
+                self.typingUsersIndicator.text = ""
+            }
+        }
         
         if AuthService.instance.isLoggedIn {
             AuthService.instance.findUserByEmail { (success) in
@@ -84,8 +125,23 @@ class ChatVC: UIViewController {
                 if success {
                     self.messageText.text = ""
                     self.messageText.resignFirstResponder()
+                    SocketService.instance.manager.defaultSocket.emit("stopType", UserDataService.instance.name, channelId)
                 }
             }
+        }
+    }
+    @IBAction func messageTextEditting(_ sender: Any) {
+        guard let channelId = MessageService.instance.selectedChannel?.id else {   return   }
+        if messageText.text == "" {
+            isTyping = false
+            messageSendButton.isHidden = true
+            SocketService.instance.manager.defaultSocket.emit("stopType", UserDataService.instance.name, channelId)
+        } else {
+            if isTyping == false {
+                messageSendButton.isHidden = false
+            SocketService.instance.manager.defaultSocket.emit("startType", UserDataService.instance.name, channelId)
+            }
+            isTyping = true
         }
     }
 }
