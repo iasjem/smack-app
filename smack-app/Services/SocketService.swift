dedd0562dbc4f2ddd9bc236d8074519f447c4eae
jem.sai <jem.sai@whitecloak.com>
2018-07-25 15:30:51 +0800

    SocketService - added socket service functionality

--- smack-app/Services/SocketService.swift
+++ smack-app/Services/SocketService.swift
@@ -7,7 +7,46 @@
 //
 
 import UIKit
+import SocketIO
 
 class SocketService: NSObject {
 
+    static let instance = SocketService()
+    
+    override init() {
+        super.init()
+    }
+    
+    let manager = SocketManager(socketURL: URL(string: BASE_URL)!, config: [.log(true), .compress])
+    
+    func establishConnection() {
+        manager.defaultSocket.connect()
+    }
+    
+    func closeConnection() {
+        manager.defaultSocket.disconnect()
+    }
+    
+    func addChannel(name: String, description: String, completion: @escaping CompletionHandler) {
+        manager.defaultSocket.emit(SOCKET_EVENT_NEW_CHANNEL, name, description)
+        completion(true)
+    }
+    
+    func getChannel(completion: @escaping CompletionHandler) {
+        manager.defaultSocket.on(SOCKET_EVENT_CHANNEL_CREATED) { (dataArray, ack) in
+            guard let name = dataArray[0] as? String else { return }
+            guard let description = dataArray[1] as? String else { return }
+            guard let id = dataArray[2] as? String else { return }
+            let newChannel = Channel(channelTitle: name, channelDescription: description, id: id)
+            MessageService.instance.channels.append(newChannel)
+            completion(true)
+        }
+    }
+    
+    func addMessage(messageBody: String, userId: String, channelId: String, completion: @escaping CompletionHandler) {
+        let user = UserDataService.instance
+        manager.defaultSocket.emit(SOCKET_EVENT_NEW_MESSAGE, messageBody, userId, channelId, user.name, user.avatarName, user.avatarColor)
+        completion(true)
+    }
+    
 }
