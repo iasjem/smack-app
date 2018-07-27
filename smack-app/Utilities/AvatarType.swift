d57734e3ef42f919dd7413f2a026157e42afad98
jem.sai <jem.sai@whitecloak.com>
2018-07-27 15:26:48 +0800

    Project - create new file

---
+++ smack-app/Utilities/AvatarType.swift
@@ -0,0 +1,14 @@
+//
+//  AvatarType.swift
+//  smack-app
+//
+//  Created by Jemimah Beryl M. Sai on 27/07/2018.
+//  Copyright © 2018 Jemimah Beryl M. Sai. All rights reserved.
+//
+
+import Foundation
+
+enum AvatarType {
+    case dark
+    case light
+}
