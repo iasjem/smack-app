428a84c4adf1db75b2fd0bbcef6f40a62411aaf0
jem.sai <jem.sai@whitecloak.com>
2018-07-27 15:38:55 +0800

    MessageService - changed if let to guard let statement

--- smack-app/Services/MessageService.swift
+++ smack-app/Services/MessageService.swift
@@ -27,16 +27,15 @@ class MessageService {
         Alamofire.request(URL_GET_CHANNELS, method: .get, parameters: nil, encoding: JSONEncoding.default, headers: BEARER_HEADER).responseJSON { (response) in
             if response.result.error == nil {
                 guard let data = response.data else {   return  }
-                if let json = JSON(data).array {
-                    for item in json {
-                        let name = item["name"].stringValue
-                        let description = item["description"].stringValue
-                        let id = item["_id"].stringValue
-                        let channel = Channel(channelTitle: name, channelDescription: description, id: id)
-                        self.channels.append(channel)
-                    }
-                    NotificationCenter.default.post(name: NOTIF_CHANNELS_LOADED, object: nil)
+                guard let json = JSON(data).array else {   return   }
+                for item in json {
+                    let name = item["name"].stringValue
+                    let description = item["description"].stringValue
+                    let id = item["_id"].stringValue
+                    let channel = Channel(channelTitle: name, channelDescription: description, id: id)
+                    self.channels.append(channel)
                 }
+                NotificationCenter.default.post(name: NOTIF_CHANNELS_LOADED, object: nil)
                 completion(true)
             } else {
                 completion(false)
@@ -49,21 +48,20 @@ class MessageService {
         Alamofire.request("\(URL_GET_MESSAGES)\(channelId)", method: .get, parameters: nil, encoding: JSONEncoding.default, headers: BEARER_HEADER).responseJSON { (response) in
             if response.result.error == nil {
                 self.clearMessages()
-                guard let data = response.data else {   return  }
-                if let json = JSON(data).array {
-                    for item in json {
-                        let messageBody = item["messageBody"].stringValue
-                        let channelId = item["channelId"].stringValue
-                        let id = item["_id"].stringValue
-                        let userName = item["userName"].stringValue
-                        let userAvatar = item["userAvatar"].stringValue
-                        let userAvatarColor = item["userAvatarColor"].stringValue
-                        let timeStamp = item["timeStamp"].stringValue
-                        let message = Message(message: messageBody, userName: userName, channelId: channelId, userAvatar: userAvatar, userAvatarColor: userAvatarColor, id: id, timeStamp: timeStamp)
-                        self.messages.append(message)
-                    }
-                    completion(true)
+                guard let data = response.data else {  return  }
+                guard let json = JSON(data).array else {  return  }
+                for item in json {
+                    let messageBody = item["messageBody"].stringValue
+                    let channelId = item["channelId"].stringValue
+                    let id = item["_id"].stringValue
+                    let userName = item["userName"].stringValue
+                    let userAvatar = item["userAvatar"].stringValue
+                    let userAvatarColor = item["userAvatarColor"].stringValue
+                    let timeStamp = item["timeStamp"].stringValue
+                    let message = Message(message: messageBody, userName: userName, channelId: channelId, userAvatar: userAvatar, userAvatarColor: userAvatarColor, id: id, timeStamp: timeStamp)
+                    self.messages.append(message)
                 }
+                completion(true)
             } else {
                 completion(false)
                 debugPrint(response.result.error as Any)
