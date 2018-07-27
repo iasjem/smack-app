c0231a6099dd2f27517325f57cc9b9faa7fbfbc9
jem.sai <jem.sai@whitecloak.com>
2018-07-27 19:51:56 +0800

    UIVC+Extension - refactored code and renamed file to VC+Extension

--- smack-app/Utilities/VC+Extension.swift
+++ smack-app/Utilities/VC+Extension.swift
@@ -10,8 +10,8 @@ import Foundation
 import UIKit
 
 extension UIViewController {
-    func showAlert(withMessage message: String) {
-        let alert = UIAlertController(title: "Alert", message: message, preferredStyle: .alert)
+    func showAlert(withTitle title: String, withMessage message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "dismiss", style: .cancel, handler: nil))
         self.present(alert, animated: true, completion: nil)
     }
